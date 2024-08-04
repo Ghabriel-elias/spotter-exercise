222c90b6787e3bab6c23a2100b5b769a087a0041
ghabriel <ghsouza1013@gmail.com>
2024-08-04 18:34:54 -0400

    feat: change in loading

--- src/Components/Loading/index.tsx
+++ src/Components/Loading/index.tsx
@@ -1,10 +1,9 @@
-import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
+import { Box, CircularProgress } from '@mui/material';
 
 export const Loading = () => {
   return (
-    <Box sx={{ width: '100%', height: '100%' }}>
-      <LinearProgress />
+    <Box sx={{width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <CircularProgress />
     </Box>
   );
 }
