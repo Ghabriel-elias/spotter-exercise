f04cc7094509f5b51c82aeb9cef4589697175982
ghabriel <ghsouza1013@gmail.com>
2024-08-03 20:09:17 -0400

    feat: change primary color

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -2,13 +2,13 @@ export default {
   colors: {
     lightColor: '#FFFFFF',
     darkColor: "#000000",
-    primaryColor: '#4CAF50',
+    primaryColor: '#216DA3',
     bgTable: '#F2F2F2',
     borderColor: '#DDDDDD',
     secundaryColor: '#f9f9f9',
     colorText: '#333333',
     disableButtonColor: "#B2B2B2",
-    hoverColor: '#45a049',
+    hoverColor: '#216DA3',
     iconDisableColor: '#686868',
     shadowColor: "rgba(0, 0, 0, 0.1)"
   }
