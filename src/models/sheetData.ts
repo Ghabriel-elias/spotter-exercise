cd7ca70316eb08accc7a2bb16baafb612bd415a0
ghabriel <ghsouza1013@gmail.com>
2024-08-04 12:07:53 -0400

    feat: add interface for table data

--- src/models/sheetData.ts
+++ src/models/sheetData.ts
@@ -10,3 +10,18 @@ export interface RowData {
   mcNumber: string;
   count: string;
 }
+
+export interface TableData {
+  created_dt: string;                
+  data_source_modified_dt: string;   
+  entity_type: string;               
+  operating_status: string;        
+  legal_name: string;                
+  dba_name: string;                  
+  physical_address: string;          
+  phone: string;                     
+  usdot_number: string;              
+  mc_mx_ff_number: string;           
+  power_units: string;               
+  out_of_service_date: string;       
+}
